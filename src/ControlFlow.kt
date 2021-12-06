84e8772470309090f6bfdfa314da8ba6157a4cea
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-06 16:31:20 +0000

    loop through nums
    will cause out of bounds

--- src/ControlFlow.kt
+++ src/ControlFlow.kt
@@ -1,24 +1,6 @@
 fun main() {
-    var names : MutableList<String> = mutableListOf("Peter", "Mark", "Alice", "Paul", "Lucas", "John")
+    var nums : MutableList<Int> = mutableListOf(34, 15, 50)
 
-    names.add("Joe")
-    names.add("Alex")
-
-    for (name in names) {
-        when(name) {
-            "Lucas" -> println("What are you doing here $name?")
-            "Alice"-> println("Hi Alice. How are you?")
-            else -> println(name)
-        }
-    }
+    for(i in 0 .. nums.size)
+        println(nums[i])
 }
-
-/*
-
-        if(name.equals("Lucas"))
-            println("What are you doing here $name?")
-        else if (name.startsWith("A"))
-            println("Your name starts with a A $name?")
-        else
-            println(name)
- */
