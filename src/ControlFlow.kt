26e5f18d458002b57d48e87c2851417dc2a8d710
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-06 15:20:05 +0000

    check if name starts with letter A

--- src/ControlFlow.kt
+++ src/ControlFlow.kt
@@ -2,10 +2,13 @@ fun main() {
     var names : MutableList<String> = mutableListOf("Peter", "Mark", "Alice", "Paul", "Lucas", "John")
 
     names.add("Joe")
+    names.add("Alex")
 
     for (name in names) {
         if(name.equals("Lucas"))
-            println("What are you doing here Lucas?")
+            println("What are you doing here $name?")
+        else if (name.startsWith("A"))
+            println("Your name starts with a A $name?")
         else
             println(name)
     }
