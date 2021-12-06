b61c54fa3b40a0f5e0c6b0cd64c8713a4ebd04d0
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-06 15:44:16 +0000

    when statement

--- src/ControlFlow.kt
+++ src/ControlFlow.kt
@@ -5,11 +5,20 @@ fun main() {
     names.add("Alex")
 
     for (name in names) {
+        when(name) {
+            "Lucas" -> println("What are you doing here $name?")
+            "Alice"-> println("Hi Alice. How are you?")
+            else -> println(name)
+        }
+    }
+}
+
+/*
+
         if(name.equals("Lucas"))
             println("What are you doing here $name?")
         else if (name.startsWith("A"))
             println("Your name starts with a A $name?")
         else
             println(name)
-    }
-}
+ */
