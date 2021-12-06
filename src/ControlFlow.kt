36acc46edd09af34a8b8f053ee8bcbd8cc61dd4a
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-06 14:36:41 +0000

    added some names and looping through them using for-each style loop

--- src/ControlFlow.kt
+++ src/ControlFlow.kt
@@ -1,3 +1,14 @@
 fun main() {
-    
+    var names : ArrayList<String> = ArrayList();
+
+    names.add("Peter")
+    names.add("Mark")
+    names.add("Alice")
+    names.add("Paul")
+    names.add("Lucas")
+    names.add("John")
+
+    for (name in names) {
+        println(name)
+    }
 }
